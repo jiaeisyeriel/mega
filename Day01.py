d95d3769333f3fdc0af083f1dd0b2811371eabfd
박한 <bagkan380@gmail.com>
2023-06-04 14:12:12 +0900

    ..

---
+++ Day01.py
@@ -0,0 +1,19 @@
+#변수(varivables)
+# 메모리 저장 공간
+# 바뀌는 데이터를 변수에 넣습니다.
+# 변수에는 세 가지 단계가 있음
+# 변수 선언(쓸거야)- 초기화(세팅해줘) - 할당(다시 세팅해줘)
+dollar = 10 # 10을 달러에 넣는다.(선언 및 초기화)
+
+print(dollar)
+
+# print() 함수
+# [입력값 - 알고리즘으로 - 결과값]
+# 함수 특징은 ~~~ () 형태이다
+# print() print 함수
+# ex) print(입력값), abc(입력값), hungry(입력값)
+# 결과값은 있을 수도 없을 수도 있다.
+
+print("hello world" + dollar)
+print(f"hellow world {dollar}")
+
